9699f3dbb7845a0ccea01eb318273b4c6daa91b9
matthewmagee05 <matthewmagee05@gmail.com>
2018-03-02 12:36:35 -0900

    updates config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
 		title: 'Gatsby Default Starter',
 		desc: 'A new blog'
 	},
-	pathPrefix: '/gatsby',
+	pathPrefix: '/gatsby-blog-test',
 	plugins: [
 		'gatsby-plugin-react-helmet',
 		'gatsby-plugin-styled-components',
