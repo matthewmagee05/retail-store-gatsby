ddb3721b8d5f3fb66a102e890988fb915e479413
matthewmagee05 <matthewmagee05@gmail.com>
2018-03-03 10:00:57 -0900

    configures netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,35 @@
 module.exports = {
-	siteMetadata: {
-		title: 'Gatsby Default Starter',
-		desc: 'A new blog'
-	},
-	plugins: [
-		'gatsby-plugin-react-helmet',
-		'gatsby-plugin-styled-components',
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				name: 'src',
-				path: `${__dirname}/src/`
-			}
-		},
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				name: 'img',
-				path: `${__dirname}/src/images`
-			}
-		},
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				name: 'img',
-				path: `${__dirname}/static/assets`
-			}
-		},
-		{
-			resolve: 'gatsby-transformer-remark',
-			options: {
-				excerpt_separator: `<!-- end -->`
-			}
-		},
-		'gatsby-transformer-sharp',
-		'gatsby-plugin-sharp',
-		'gatsby-plugin-netlify-cms'
-	]
+  siteMetadata: {
+    title: "Gatsby Default Starter",
+    desc: "A new blog"
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "img",
+        path: `${__dirname}/src/images`
+      }
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        excerpt_separator: `<!-- end -->`
+      }
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-netlify-cms"
+  ]
 };
 
 // pathPrefix: "/levelupgatsby",
