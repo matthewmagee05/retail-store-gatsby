1b81cc048a16b12e7dfee593f53ce153d5a6a451
matthewmagee05 <matthewmagee05@gmail.com>
2018-03-03 10:11:35 -0900

    configures netlify cms

--- src/posts/PostPage.js
+++ src/posts/PostPage.js
@@ -18,17 +18,13 @@ export default class PostPage extends Component {
 }
 
 export const query = graphql`
-	query BlogPostQuery($slug: String!){
-		  markdownRemark(fields: { 
-		    slug: { 
-		      eq: $slug
-		    }
-		  }) {
-		    html
-		    frontmatter{
-		    	title
-		    	date(formatString:"MMMM DD YYYY")
-		    }
-		  }
-		}
+  query BlogPostQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD YYYY")
+      }
+    }
+  }
 `;
